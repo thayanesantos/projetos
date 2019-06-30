3181d8caa27ca734d59c4cb0ad62a6d72e137a90
tmsantos27 <tmsantos.web@gmail.com>
2019-06-29 23:40:04 -0300

    06/2019

---
+++ iniciando/script.js
@@ -0,0 +1,116 @@
+// VALIDAÇÃO DE FORM
+
+
+// function valida (){
+//	var nome = document.getElementById('nome');
+//
+//	if (nome.value == "") {
+//		alert("Preencha o campo em branco!")
+//	}
+//	else {
+//		alert("Nenhum problema detectado!")
+//	}
+//	// alert(nome.value);
+//}
+
+
+
+
+// backgroundColor
+// function mudaCor(cor){
+// 	var elemento = document.getElementById("mensagem")
+// 	elemento.style.backgroundColor = cor;
+// }
+
+// Color
+function mudaCor(cor){
+	var elemento = document.getElementById("mensagem")
+	elemento.style.color = cor;
+}
+
+// font size
+// function mudaCor(cor){
+// 	var elemento = document.getElementById("mensagem")
+// 	elemento.style.fontSize = "50px";
+// }
+
+//EVENTO  (onclick)
+// function mensagem(nome){
+// 	alert("Não clique em mim!"+nome)
+
+// }
+
+
+//FUNÇÕES
+// function soma (a,b) {
+// 	console.log (a+b);
+// }
+
+// //soma (2, 2);
+
+// function subtracao (a, b){
+// 	return a-b;
+// }
+
+// var s = subtracao (5,3);
+// console.log(s);
+
+
+
+// ARRAYS (matrizes)
+
+// var numero = 6;
+// var decimal = 4.5;
+// var texto = "Olá mundo";
+// var lista = ["laranja", "maçã", "banana"]
+
+// // alert(lista[0]);
+
+// for (i in lista){
+// 	alert(lista[i]);
+// }
+
+
+
+
+
+// ESTRUTURAS CONDICIONAIS//
+
+// var a = 2;
+// var b = 3;
+// var c = "Hello World";
+
+// var soma = a+b;
+// var sub = b-a;
+// var mult = b*a;
+
+// // alert(soma);
+// // alert(sub);
+// // alert(mult);
+
+// if (c%2 == 1) {
+// 	alert("Número ímpar")
+// }
+// else if (c%2 == 0) {
+// 	alert("Número par")
+// }
+// else {
+// 	alert("Número inválido")
+// }
+
+
+// ESTRUTURAS DE REPETIÇÃO //ESTRUTURA WHILE(ENQUANTO)//
+
+// var i = 0;
+
+// while (i < 3){
+// 	alert(i);
+// 	i = i+1;
+// }
+
+// // for (para)// //
+
+// for (var j = 0; j < 3; j++){
+// 	alert(j);
+// }
+
