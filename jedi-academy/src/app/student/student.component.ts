7ff679ea6a0dad69b889048c5d9f33a001285a4e
tmsantos27 <tmsantos.web@gmail.com>
2020-04-27 15:38:43 -0300

    update

---
+++ jedi-academy/src/app/student/student.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Student } from './student.model'
+
+@Component({
+  selector: 'jad-student',
+  templateUrl: './student.component.html',
+  styleUrls: ['./student.component.scss']
+})
+export class StudentComponent implements OnInit {
+
+  @Input() student:Student 
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
